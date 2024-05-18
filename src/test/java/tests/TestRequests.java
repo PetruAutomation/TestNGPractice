4bf49395975c7a2ad914bae2a3dcc3ed4ccff932
PetruAutomation <petru.pascari@orange.com>
2024-05-18 12:41:03 +0300

    added fist test

--- src/test/java/tests/TestRequests.java
+++ src/test/java/tests/TestRequests.java
@@ -2,11 +2,18 @@ package tests;
 
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class TestRequests {
 
 
     @Test
-    public void testTestNG(){
-        System.out.println("Hello World");
+    public void testListUsers(){
+        given()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .assertThat()
+                .statusCode(200);
     }
 }
