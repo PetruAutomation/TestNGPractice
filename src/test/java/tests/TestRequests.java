6a80fb6c678a16b77e80070fa264ec8927123674
PetruAutomation <petru.pascari@orange.com>
2024-05-18 11:25:00 +0300

    init commit

---
+++ src/test/java/tests/TestRequests.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class TestRequests {
+
+
+    @Test
+    public void testTestNG(){
+        System.out.println("Hello World");
+    }
+}
